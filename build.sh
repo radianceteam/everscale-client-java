e152e09991875a1315d6ff2727021574342922f3
Igor Vasilev <igorvasiliev@mail.ru>
2021-09-13 18:48:31 +0300

    v1.21.4

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 tondir=target/TON-SDK
 
 projectdir=`pwd`
-branch=1.21.2
+branch=1.21.4
 #`git rev-parse --abbrev-ref HEAD`
 
 [ -d "$tondir" ] || git clone --single-branch --branch $branch https://github.com/tonlabs/TON-SDK.git $tondir
